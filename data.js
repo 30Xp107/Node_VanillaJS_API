86555a032e8ca77ebf7ef14e937b53f92f3c51d5
30Xpl107 <jhanbrent0630@gmail.com>
2023-06-04 22:27:39 +0800

    *

---
+++ data.js
@@ -0,0 +1,28 @@
+const todos = [
+    {
+        id: 1,
+        title: "Coding in Javascirpt",
+        description: "Working with functions in javascript",
+        completed: false,
+    },
+    {
+        id:2,
+        title: "Cooling Supper",
+        description: "preparing rice and chicker",
+        completed: false,
+    },
+    {
+        id:3,
+        title: "Taking a walk",
+        description: "Easy time at the park",
+        completed: false,
+    },
+    {
+        id:4,
+        title: "Watching Netflix",
+        description: "Easy time at the park",
+        completed: false,
+    },
+];
+
+module.exports = todos;
